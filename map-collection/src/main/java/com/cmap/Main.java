106a35295f2df25c022be8b1c359e8aa684cd67f
David Dominguez <dominguezdavid396gmail.com>
2024-03-12 12:43:11 -0600

    Factory en main

--- map-collection/src/main/java/com/cmap/Main.java
+++ map-collection/src/main/java/com/cmap/Main.java
@@ -19,32 +19,25 @@ public class Main {
         System.out.println("3. LinkedHashMap");
 
         // Lee la opción del usuario
-        int opcion = scanner.nextInt();
-        scanner.nextLine(); // Limpiar el buffer
+        String opcion = scanner.nextLine();
 
         long inicio = System.nanoTime();
-        // Dependiendo de la opción elegida, inicializa el tipo de mapa correspondiente
-        switch (opcion) {
-            case 1:
-                System.out.println("HasMap");
-                IMap<String, String> cHashMap = new cHashMap<>();
-                inicializarMapa(cHashMap, datos);
-                break;
-            case 2:
-                System.out.println("TreeMap");
-                IMap<String, String> cTreeMap = new cTreeMap<>();
-                inicializarMapa(cTreeMap, datos);
-                break;
-            case 3:
-                System.out.println("LinkedHashMap");
-                IMap<String, String> linkedHM = new LinkedHM<>();
-                inicializarMapa(linkedHM, datos);
-                break;
-            default:
-                System.out.println("Opción no válida");
+
+        // Utiliza el FactoryMap para crear el tipo de mapa correspondiente
+        FactoryMap<String, String> factoryMap = new FactoryMap<>();
+        IMap<String, String> mapa = factoryMap.createMap(opcion);
+
+        
+        if (mapa == null) {
+            System.out.println("Opción no válida");
+        } else {
+            inicializarMapa(mapa, datos);
         }
+
         long fin = System.nanoTime();
         long tiempoTranscurrido = fin - inicio;
+        System.out.println("Tiempo Inicio: " + inicio);
+        System.out.println("Tiempo Fin: " + fin);
         System.out.println("Tiempo transcurrido: " + tiempoTranscurrido + " nanosegundos");
     }
 
