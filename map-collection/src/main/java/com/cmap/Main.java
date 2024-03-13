f5929187688e7edf51dc9ed6c8daaeebf8faf40e
David Dominguez <dominguezdavid396gmail.com>
2024-03-12 20:50:30 -0600

    Implementacion de opciones 1 2 3 5 7 del menu

--- map-collection/src/main/java/com/cmap/Main.java
+++ map-collection/src/main/java/com/cmap/Main.java
@@ -1,12 +1,16 @@
 package com.cmap;
+
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
         // Lee los datos del archivo
-        List<String[]> datos = lector.leerArchivo("/Users/alejandraayala/Desktop/EstructuraDatos/HDT6-MAP/map-collection/src/main/java/com/cmap/cards_desc.txt");
+        List<String[]> datos = lector.leerArchivo("map-collection\\src\\main\\java\\com\\cmap\\cards_desc.txt");
 
         // Crea el scanner para la entrada del usuario
         Scanner scanner = new Scanner(System.in);
@@ -20,27 +24,20 @@ public class Main {
         // Lee la opción del usuario
         String opcion = scanner.nextLine();
 
-        long inicio = System.nanoTime();
-
         // Utiliza el FactoryMap para crear el tipo de mapa correspondiente
         FactoryMap factoryMap = new FactoryMap();
         IMap mapa = factoryMap.createMap(opcion);
 
         if (mapa == null) {
             System.out.println("Opción no válida");
+            return;
         } else {
             inicializarMapa(mapa, datos);
         }
 
-        long fin = System.nanoTime();
-        long tiempoTranscurrido = fin - inicio;
-        System.out.println("Tiempo Inicio: " + inicio);
-        System.out.println("Tiempo Fin: " + fin);
-        System.out.println("Tiempo transcurrido: " + tiempoTranscurrido + " nanosegundos");
-
         // Menú de operaciones para el usuario
-        boolean salir = true;
-        while (salir) {
+        boolean salir = false;
+        while (!salir) {
             System.out.println("Seleccione una opción:");
             System.out.println("1. Agregar una carta a la colección del usuario");
             System.out.println("2. Mostrar el tipo de una carta específica");
@@ -54,19 +51,46 @@ public class Main {
 
             switch (option) {
                 case "1":
+                    System.out.println("Ingrese el nombre de la carta:");
+                    String nombre = scanner.nextLine();
+                    System.out.println("Ingrese el tipo de la carta:");
+                    String tipo = scanner.nextLine();
+                    mapa.addCard(nombre, tipo);
+                    break;
                 case "2":
+                    System.out.println("Ingrese el nombre de la carta:");
+                    String carta = scanner.nextLine();
+                    String tipoCarta = mapa.getCardType(carta);
+                    if (tipoCarta != null) {
+                        System.out.println("El tipo de la carta " + carta + " es " + tipoCarta);
+                    } else {
+                        System.out.println("La carta " + carta + " no existe en la colección.");
+                    }
+                    break;
                 case "3":
+                    Map<String, Integer> coleccionUsuario = mapa.getUserCollection();
+                    for (Map.Entry<String, Integer> entry : coleccionUsuario.entrySet()) {
+                        System.out.println("Nombre: " + entry.getKey() + ", Tipo: " + mapa.getCardType(entry.getKey()) + ", Cantidad: " + entry.getValue());
+                    }
+                    break;
                 case "4":
+                    break;
                 case "5":
+                    List<String> todasLasCartas = mapa.getAllCards();
+                    for (String cartaNombre : todasLasCartas) {
+                        System.out.println("Nombre: " + cartaNombre + ", Tipo: " + mapa.getCardType(cartaNombre));
+                    }
+                    break;
                 case "6":
+                    break;
                 case "7":
-                    salir = false;
+                    salir = true;
                     break;
                 default:
                     System.out.println("Opción no válida.");
             }
         }
-    scanner.close();
+        scanner.close();
     }
 
     public static void inicializarMapa(IMap mapa, List<String[]> datos) {
